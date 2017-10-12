eed8000bbeec59a903cc4b4d0388cae4b9d4035e
Rob Uttley <rob@robsoft.net>
2017-10-12 12:21:18 +0100

    Added NUnit stuff :-)

---
+++ PerilousMobile/PerilousMobile.Tests.Droid/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using NUnit.Framework;
+using Android.Content.PM;
+using NUnit.Runner.Services;
+
+namespace PerilousMobile.Tests.Droid
+{
+    [Activity(Label = "NUnit 3", MainLauncher = true, Theme = "@android:style/Theme.Holo.Light", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+            // This will load all tests within the current project
+            var nunit = new NUnit.Runner.App();
+
+            // If you want to add tests in another assembly
+            //nunit.AddTestAssembly(typeof(MyTests).Assembly);
+
+            // Do you want to automatically run tests when the app starts?
+            nunit.Options = new TestOptions
+            {
+                AutoRun = true
+            };
+
+            LoadApplication(nunit);
+        }
+    }
+}
---
+++ PerilousMobile/PerilousMobile.Tests/TestWrapperClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using PerilousMobile;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace PerilousMobile.Tests
+{
+
+
+    [TestFixture]
+    public class TestWrapperClass : AssertionHelper
+    {
+        private Game game;
+
+        [SetUp]
+        protected void Setup()
+        {
+            game = new Game();
+        }
+
+        [Test]
+        public void IsNull()
+        {
+            object nada = null;
+            Assert.IsNull(nada);
+
+            Assert.That(nada, Is.Null);
+
+            Expect(nada, Null);
+        }
+
+        [Test]
+        public void CheckGameHealthPointsOver10()
+        {
+            Assert.That(game.healthPoints, Is.GreaterThan(10));
+            Assert.That(game.healthPoints, Is.LessThan(10));
+        }
+    }
+}
